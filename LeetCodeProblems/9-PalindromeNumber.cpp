e29bfa1f2eba8e69c49aafe78af2ec6e47814bb5
rodnoycry <vkhk99@gmail.com>
2026-01-19 22:43:40 +0700

    Finished first version of the solution

--- LeetCodeProblems/9-PalindromeNumber.cpp
+++ LeetCodeProblems/9-PalindromeNumber.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <cstdlib>
 
 #define LOG(x) std::cout << x << std::endl
 
 using std::vector;
 
 namespace PalindromeNumber {
+    // Find out the length of array of decimal digits we need to reserve in array
     constexpr int maxIntDigits = std::numeric_limits<int>::digits10 + 1;
 
     class Solution {
     public:
         bool isPalindrome(int x) {
+            // Negative numbers does not fit because of "-" symbol in the start
+            if (x < 0) return false;
+
             int digits[maxIntDigits];
-            return false;
+            int digitsLength = 0;
+
+            int remainingValue = x;
+
+            do {
+                std::div_t result = std::div(remainingValue, 10);
+                // Appending extracted digit to an array of digits
+                digits[digitsLength] = result.rem;
+                digitsLength++;
+                // Assigning "remaining" value to processing variable
+                remainingValue = result.quot;
+            } while (remainingValue != 0); // Remaining value 0 means we split whole nubmer into decimal digits
+
+            for (unsigned int i = 0; i < digitsLength / 2; i++) {
+                if (digits[i] != digits[digitsLength - 1 - i]) {
+                    return false;
+                };
+            };
+            return true;
         }
     };
 
@@ -30,7 +53,8 @@ namespace PalindromeNumber {
             { 1234, false },
             { 121, true },
             { 123454321, true },
-            { -123454321, true },
+            // All negative numbers falsy because of "-" sign
+            { -123454321, false },
             { -6767621, false },
         };
 

7c6dc19590b6600c10a381428487faa89d205714
rodnoycry <vkhk99@gmail.com>
2026-01-26 20:05:05 +0700

    First attempt of solution - using bitwise operation to get only characters that are the same in strings, then resize output and return result

--- LeetCodeProblems/14-LongestCommonPrefix.cpp
+++ LeetCodeProblems/14-LongestCommonPrefix.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 #include "TestUtils.h"
 
 #define LOG(x) std::cout << x << std::endl
@@ -9,37 +10,94 @@ using std::vector;
 using std::string;
 
 namespace LongestCommonPrefix {
-
     class Solution {
     public:
-        string longestCommonPrefix(const vector<string>& strs) {
-            string commonPrefix = "";
-            return commonPrefix;
+        string longestCommonPrefix(vector<string>& strs) {
+            string& lastString = strs[0];
+            for (size_t i = 1; i < strs.size(); i++) {
+                string& currentString = strs[i];
+                size_t leastStrLength = std::min(lastString.length(), currentString.length());
+
+                for (size_t i = 0; i < leastStrLength; ++i) {
+                    // A bit of bitwise masking to get only repeating strings and \0 at 
+                    // all other places
+                    lastString[i] = lastString[i] & -(lastString[i] == currentString[i]);
+                }
+            }
+            // We need to resize the thing because we will have something like "pre\0\0s\0" and
+            // we clear everything till first null terminator character by treating it as regular string
+            // and we get pointer to the data by .data() and strlen to get length till first null
+            // terminator character
+            lastString.resize(strlen(lastString.data()));
+            return lastString;
         }
     };
 
     struct TestCase {
-        const std::vector<std::string>& strings;
-        const std::string& answer;
+        std::vector<std::string> strings;
+        std::string answer;
     };
+
+    // Making \0 visible in logs
+    static std::string visualizeString(const std::string& s) {
+        std::string res = "";
+        for (char c : s) {
+            if (c == '\0') res += "\\0";
+            else res += c;
+        }
+        return res;
+    }
 
     void test() {
         Solution solution;
 
         std::vector<TestCase> testCases = {
+            // 1. The "Standard" case
+            { {"flower", "flow", "flight"}, "fl" },
+
+            // 2. No common prefix at all (should result in an empty string)
+            { {"dog", "racecar", "car"}, "" },
+
+            // 3. Only one string in the vector (The prefix is the string itself)
+            { {"single"}, "single" },
+
+            // 4. The first string is very long, others are short
+            // (Crucial for your "in-place" logic!)
+            { {"abracadabra", "ab", "abc"}, "ab" },
+
+            // 5. The strings are identical
+            { {"test", "test", "test"}, "test" },
+
+            // 6. One of the strings is empty
+            { {"", "b", "c"}, "" },
+            { {"a", "", "c"}, "" },
+
+            // 7. The common prefix is the entire first string
+            { {"low", "lower", "lowest"}, "low" },
+
+            // 8. Strings with only one character (matching)
+            { {"a", "a", "a"}, "a" },
+
+            // 9. Strings with only one character (not matching)
+            { {"a", "b", "c"}, "" }
         };
 
-        TestUtils::runTests(testCases, [&](TestCase testCase) {
-            std::string result = solution.longestCommonPrefix(
-                testCase.strings
-            );
+        TestUtils::runTests(testCases, [&](TestCase& testCase) {
+            std::string result = solution.longestCommonPrefix(testCase.strings);
+
+            // Format the strings to see null terminator characters
+            std::string visualResult = visualizeString(result);
+            std::string visualAnswer = visualizeString(testCase.answer);
+
             if (result == testCase.answer) {
+                LOG("[SUCCESS] \"" << visualResult << "\" == \"" << visualAnswer << "\"");
                 return true;
             }
             else {
+                LOG("[FAIL] \"" << visualResult << "\" != \"" << visualAnswer << "\"");
                 return false;
             }
-            });
+        });
     }
 
 }

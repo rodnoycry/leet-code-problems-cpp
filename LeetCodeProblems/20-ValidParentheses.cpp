6d9972e0520f2e15cd1b516d64ab302396bfeb1d
rodnoycry <vkhk99@gmail.com>
2026-01-27 16:13:40 +0700

    First attempt, arithmetic approach

--- LeetCodeProblems/20-ValidParentheses.cpp
+++ LeetCodeProblems/20-ValidParentheses.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 #include "TestUtils.h"
 
 #define LOG(x) std::cout << x << std::endl
@@ -9,15 +10,45 @@ using std::vector;
 using std::string;
 
 namespace ValidParentheses {
+    std::unordered_map<char, int> openingCharsMap = {
+        {'(', 1},
+        {'[', 10},
+        {'{', 100},
+    };
+
+    std::unordered_map<char, int> closingCharsMap = {
+        {')', -1},
+        {']', -10},
+        {'}', -100},
+    };
+
     class Solution {
     public:
-        bool isValid(string s) {
-            return false;
+        bool isValid(const string& s) {
+            int sum = 0;
+            int lastCharValue = 0;
+            for (const char c : s) {
+                int value;
+                auto it = openingCharsMap.find(c);
+                if (it != openingCharsMap.end()) {
+                    value = it->second; 
+                }
+                else {
+                    value = closingCharsMap[c];
+                }
+                if (value < 0) {
+                    if ((lastCharValue + value) != 0) return false;
+                    sum -= value;
+                }
+                lastCharValue = value;
+                sum += value;
+            }
+            return sum == 0;
         }
     };
 
     struct TestCase {
-        std::string testString;
+        const std::string testString;
         bool answer;
     };
 
@@ -25,14 +56,23 @@ namespace ValidParentheses {
         Solution solution;
 
         std::vector<TestCase> testCases = {
+            { "()", true },
+            { "[({})]", true },
+            { "[](){}", true },
+            { "[]({}){}", true },
+            { "[]({}}){}", false },
+            { "[(}]", false },
+            { "(((((((((((]", false },
         };
 
         TestUtils::runTests(testCases, [&](TestCase testCase) {
             bool result = solution.isValid(testCase.testString);
             if (result == testCase.answer) {
+                LOG("[Success] " << testCase.testString << " validated as " << result);
                 return true;
             }
             else {
+                LOG("[Failed] " << testCase.testString << " validated as " << result);
                 return false;
             }
             });

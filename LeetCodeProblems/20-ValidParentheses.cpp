db2598027ba9722cc919dff0bd22cba9a41c4190
rodnoycry <vkhk99@gmail.com>
2026-01-27 16:53:08 +0700

    Reworked solution to  use "stack-alike" approach to keep track of unhandled parentheses

--- LeetCodeProblems/20-ValidParentheses.cpp
+++ LeetCodeProblems/20-ValidParentheses.cpp
@@ -10,40 +10,31 @@ using std::vector;
 using std::string;
 
 namespace ValidParentheses {
-    std::unordered_map<char, int> openingCharsMap = {
-        {'(', 1},
-        {'[', 10},
-        {'{', 100},
-    };
-
-    std::unordered_map<char, int> closingCharsMap = {
-        {')', -1},
-        {']', -10},
-        {'}', -100},
-    };
-
     class Solution {
+    private:
+        std::unordered_map<char, int> m_ClosingCharsMap = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'},
+        };
     public:
         bool isValid(const string& s) {
-            int sum = 0;
-            int lastCharValue = 0;
-            for (const char c : s) {
-                int value;
-                auto it = openingCharsMap.find(c);
-                if (it != openingCharsMap.end()) {
-                    value = it->second;
+            std::string charsStack;
+            charsStack.reserve(s.length());
+
+            for (char c : s) {
+                auto it = m_ClosingCharsMap.find(c);
+                if (it != m_ClosingCharsMap.end()) {
+                    if (charsStack.empty() || charsStack.back() != it->second) {
+                        return false;
+                    }
+                    charsStack.pop_back();
                 }
                 else {
-                    value = closingCharsMap[c];
+                    charsStack.push_back(c);
                 }
-                if (value < 0) {
-                    int temp = lastCharValue + value;
-                    if ((lastCharValue + value) != 0) return false;
-                }
-                lastCharValue = value;
-                sum += value;
             }
-            return sum == 0;
+            return charsStack.empty();
         }
     };
 
